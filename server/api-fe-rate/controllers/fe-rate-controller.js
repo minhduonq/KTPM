30dcac009396219442d7d64a0307b7627e7f5426
Dung8229 <22028229@vnu.edu.vn>
2024-11-07 11:04:16 +0700

    Thêm ambassador cho 2 container
    - Đặt 2 file ambassadorService trong 2 container
    - ambassador có cài đặt circuit breaker, request timer và logging
    - Chưa cài đặt cache bằng redis được, chỉ có cache bằng RAM
    - Ambassador có cơ chế simulate lỗi fetch url trong hàm tryFetchData, có thể comment đi
    - Nếu cần test circuit breaker có thể comment phần cache trong hàm tryFetchData đi

--- server/api-fe-rate/controllers/fe-rate-controller.js
+++ server/api-fe-rate/controllers/fe-rate-controller.js
@@ -1,11 +1,18 @@
 //const gpModel = require('../models/fe-rate-model')
-const axios = require('axios')
+// const axios = require('axios')
 const xml2js = require('xml2js');
+const AmbassadorService = require('../ambassadorService')
+
+const feURL = 'https://portal.vietcombank.com.vn/Usercontrols/TVPortal.TyGia/pXML.aspx'
+
+// Đặt failure threshold = 3, success threshold = 2
+const ambassadorService = new AmbassadorService(feURL, 3, 2)
 
 exports.getFERate = async (req,res,next) => {
   try {
-    const response = await axios.get('https://portal.vietcombank.com.vn/Usercontrols/TVPortal.TyGia/pXML.aspx');
-    const xmlData = response.data;
+    // const response = await axios.get('https://portal.vietcombank.com.vn/Usercontrols/TVPortal.TyGia/pXML.aspx');
+    // const xmlData = response.data;
+    const xmlData = await ambassadorService.fetchData();
 
     xml2js.parseString(xmlData, (err,result) => {
       if(err) {
--- server/api-gold-price/controllers/gold-price-controller.js
+++ server/api-gold-price/controllers/gold-price-controller.js
@@ -8,13 +8,21 @@ exports.setGoldPrice = (req,res,next) => {
 }
 */
 
+const AmbassadorService = require('../ambassadorService')
+
+const goldURL = 'https://sjc.com.vn/GoldPrice/Services/PriceService.ashx'
+
+// Đặt failure threshold = 3, success threshold = 1
+const ambassadorService = new AmbassadorService(goldURL, 3, 1)
+
 exports.getGoldPrice = async (req,res,next) => {
     try {
-        const response = await axios.get('https://apiforlearning.zendvn.com/api/get-gold', {
-          headers: {
-            'accept': 'application/json'
-          }
-        });
+        // const response = await axios.get('https://apiforlearning.zendvn.com/api/get-gold', {
+        //   headers: {
+        //     'accept': 'application/json'
+        //   }
+        // });
+        const response = await ambassadorService.fetchData()
         res.json(response.data);
       } catch (error) {
         console.error(error);
